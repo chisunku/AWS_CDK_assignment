e13f0cc137d9de80ae59a46d337a7d8ed798a17a
chinmayi sunku <chinmayi.sunku@sjsu.edu>
2023-03-09 20:34:51 -0800

    final code after modification

--- lib/aws_cdk-stack.ts
+++ lib/aws_cdk-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,7 +7,12 @@ export class AwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, 'AWS-CDK-assignment-bucket', {
+      	versioned: true,
+  	removalPolicy: cdk.RemovalPolicy.DESTROY,
+  	autoDeleteObjects: true
+    });
+	// The code that defines your stack goes here
 
     // example resource
     // const queue = new sqs.Queue(this, 'AwsCdkQueue', {
